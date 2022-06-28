a5fd6bb661f2d11a02e9fd960694f2bf0aa99136
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-06-29 00:27:19 +0500

    nu-cli compiles

--- crates/nu-cli/src/lib.rs
+++ crates/nu-cli/src/lib.rs
@@ -1,32 +1,32 @@
 mod commands;
-mod completions;
-mod config_files;
+// mod completions;
+// mod config_files;
 mod eval_file;
-mod menus;
-mod nu_highlight;
+// mod menus;
+// mod nu_highlight;
 mod print;
-mod prompt;
-mod prompt_update;
-mod reedline_config;
-mod repl;
-mod syntax_highlight;
+// mod prompt;
+// mod prompt_update;
+// mod reedline_config;
+// mod repl;
+// mod syntax_highlight;
 mod util;
-mod validation;
+// mod validation;
 
 pub use commands::evaluate_commands;
-pub use completions::{FileCompletion, NuCompleter};
-pub use config_files::eval_config_contents;
+// pub use completions::{FileCompletion, NuCompleter};
+// pub use config_files::eval_config_contents;
 pub use eval_file::evaluate_file;
-pub use menus::{DescriptionMenu, NuHelpCompleter};
-pub use nu_highlight::NuHighlight;
+// pub use menus::{DescriptionMenu, NuHelpCompleter};
+// pub use nu_highlight::NuHighlight;
 pub use print::Print;
-pub use prompt::NushellPrompt;
-pub use repl::evaluate_repl;
-pub use syntax_highlight::NuHighlighter;
+// pub use prompt::NushellPrompt;
+// pub use repl::evaluate_repl;
+// pub use syntax_highlight::NuHighlighter;
 pub use util::{eval_source, gather_parent_env_vars, get_init_cwd, report_error};
-pub use validation::NuValidator;
+// pub use validation::NuValidator;
 
-#[cfg(feature = "plugin")]
-pub use config_files::add_plugin_file;
-#[cfg(feature = "plugin")]
-pub use config_files::read_plugin_file;
+
+// pub use config_files::add_plugin_file;
+// #[cfg(feature = "plugin")]
+// pub use config_files::read_plugin_file;
